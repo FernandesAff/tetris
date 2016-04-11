ff5f28651af984cc1fa4d673bc461f029d01d230
Anderson <makiander.abr@gmail.com>
2016-04-11 18:20:38 -0300

    peças melhorado

--- peças.c
+++ peças.c
@@ -3,38 +3,47 @@
 #include <time.h>       /* time */
 #include <ncurses.h>
 
-int main ()
-{
-	int x,i;	// i= contador; x= host do tamanho da peça
-	char p[4];  //peça
-	int t, sair=0, z=3, y=3;
+void gera_peca (char *p){
+	int z,i;	// i= contador; x= host do tamanho da peça
 
 	srand (time(NULL));
-	x= rand()%6;
-	if(x<=3){
-		x=3;
+	z= rand()%6;
+	if(z<=3){
+		z=3;
 	}
-	for(i=0;i<x;i++){
+	for(i=0;i<z;i++){
 		p[i]='o';
 	}
-	for(i=x;i<=5;i++){
+	for(i=z;i<=5;i++){
 		p[i]=' ';
 	}
+
+	return;
+}
+
+int main ()
+{
+	int t, sair=0, x=3, y=3;
+	char str[4];
+	
+	gera_peca(str);
 	initscr(); /* inicia ncurses */
 	cbreak(); /* recebe input sem precisar apertar <enter> */
 	noecho(); /* não mostra na tela input recebido */
 	timeout(0); /* não fica esperando por input */
 	curs_set(0); /* deixa o cursor invisivel*/
+	keypad(stdscr, TRUE);
 	while (sair == 0){
 		switch(getch()){
-			case KEY_LEFT: z--; break;
-			case KEY_RIGHT: z++; break;
+			case KEY_LEFT: x--; break;
+			case KEY_RIGHT: x++; break;
 			case KEY_DOWN: y++; break;
 			/* "asd" controlam o cursor */
 			case 'q': sair = 1; break;
 		}
 		clear();
-		mvaddstr(y, z, p);
+		
+		mvaddstr(y, x, str);
 		/* mostra char na posicao x, y */
 	}
 	endwin();
