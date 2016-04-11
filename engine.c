792df560d9e82abc99f07fa126006dc32b43a5a3
CristovaoBG <cristovao@live.com>
2016-04-11 12:29:20 -0300

    adicionada colisao

--- engine.c
+++ engine.c
@@ -1,12 +1,21 @@
 #include <ncurses.h>
 #include "tela.h"
 
+
+int posicaolivre(char pos){
+	int livre=0;
+	if (pos==' ') livre =1;
+	return(livre);
+}
+
 int loop(char tela[][25]){
 
-	int sair=0,x=0,y=0,prevx,prevy;
+	int sair=0,x=1,y=1,prevx,prevy;
 	cbreak();
 	noecho();
 	// incluir timeout() no futuro
+
+	tela[10][10]='0'; // peca para teste de colisao
 
 while (sair==0){ //mudar para setas
 	prevx=x;
@@ -14,13 +23,25 @@ while (sair==0){ //mudar para setas
 
 	switch(getch())
 	{
-		case 'a': x--; //limitar x caso em fronteira
+		case 'a': 
+			if (posicaolivre(tela[y][x-1]) && x>1)
+			x--; //limitar x caso em fronteira
 			break;	
-		case 's': y++;
+		case 's':
+			if (posicaolivre(tela[y+1][x]) && y<14)
+			y++;
+			else{
+			tela[y][x]='0'; //fixa peca
+			x=1;prevx=1;y=1,prevy=1; //''cria'' nova peca
+			}
 			break;
-		case 'd': x++;
+		case 'd': 
+			if (posicaolivre(tela[y][x+1]) && x<24)
+			x++;
 			break;
-		case 'w': y--;
+		case 'w': 
+			if (posicaolivre(tela[y-1][x]) && y>0)
+			y--; // eliminar esta opcao
 			break;
 		case 'q': sair=1;
 			break;
