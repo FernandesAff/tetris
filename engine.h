526446f68516bab049c2d9eb79cdf77638d9e3fb
pvfm <pvmonteiro26@gmail.com>
2016-06-13 10:55:27 -0300

    Add files via upload

--- engine.h
+++ engine.h
@@ -1,5 +1,5 @@
 #define TAMANHOTELAY 15
-#define TAMANHOTELAX 25 
+#define TAMANHOTELAX 25
 #include <ncurses.h>
 #include "tela.h"
 #include "pecas.h"
@@ -12,6 +12,6 @@ void AddBloco(TipoPeca *,TipoTela [][25]);
 void RemoveBloco(TipoPeca *,TipoTela [][25]);
 int VerificaLinhas(TipoTela [][TAMANHOTELAX]);
 void Gravidade(TipoTela [][TAMANHOTELAX],int);
-void Deletalinhas(TipoTela [][TAMANHOTELAX],int *);
+void DeletaLinhas(TipoTela [][TAMANHOTELAX],int *);
 int PegaInput();
 int Loop (TipoTela [][25]);
