7f212f1e369ec15a119dd79e546dcddb76b52513
pvfm <pvmonteiro26@gmail.com>
2016-06-13 10:44:38 -0300

    Add files via upload

--- pecas.h
+++ pecas.h
@@ -13,14 +13,16 @@ TipoPeca *AlocaPeca();
 void LiberaPeca(TipoPeca*);
 void CopiaPeca(TipoPeca*, TipoPeca*);
 void GeraPeca (TipoPeca *); //gera peca aleatoriamente sobre um dado ponteiro TipoPeca
-void GeraPecaEspecifica(TipoPeca *, int, int); //gera peca especifica, para teste 
+void GeraPecaEspecifica(TipoPeca *, int, int); //gera peca especifica, para teste
 void MovePecaX (TipoPeca *, int);
 void MovePecaY (TipoPeca *, int);
 void RotacionaPeca(TipoPeca*);
+void SpeedUp(TipoPeca *, int);
 int PecaGetCor(TipoPeca*);
 int PecaGetX(TipoPeca*);
 int PecaGetY(TipoPeca*);
 int PecaGetOrient(TipoPeca*);
+int PecaGetSpeed(TipoPeca *);
 TipoTela PecaGetBloco(TipoPeca*, int, int); //retorna o caractere do bloco na posicao requerida (x,y)
 
 #endif
